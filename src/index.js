559dc5b4f2dd8ec6e6e89e3726ccf1ab31d0cf92
FJF616 <franzjfernando@hotmail.com>
2018-06-09 19:40:10 -0700

    adding front end stripe elements directly from the examples provided on github

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 import registerServiceWorker from "./registerServiceWorker";
+import {StripeProvider} from 'react-stripe-elements';
 
+import MyStoreCheckout from './Components/StripeStuff/MyStoreCheckout';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import thunk from "redux-thunk";
@@ -13,18 +15,24 @@ import logger from "redux-logger";
 import rootReducer from "./Reducers/index";
 import ReduxPromise from "redux-promise";
 import App from "./App";
-
+import 
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk, ReduxPromise, logger)
 );
 
+
+
 ReactDOM.render(
   <Provider store={store}>
+  <StripeProvider apiKey="pk_test_H7uewnjg2jKXWSlnvBfFmxzo">
     <BrowserRouter>
       <App />
+   
+    <MyStoreCheckout />
     </BrowserRouter>
+    </StripeProvider>
   </Provider>,
   document.getElementById("root")
 );
